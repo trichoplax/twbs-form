3e229e6f98026c6f57c62a1273399e07df7da59f
mporam <emailme@mikeoram.co.uk>
2015-12-16 11:22:02 +0000

    update validator to use data attr

--- validate.js
+++ validate.js
@@ -1,5 +1,6 @@
 $(function() {
-    $('form').submit(function() {
+    $('form').submit(function(e) {
+        e.preventDefault()
         var submit = true
         $('.error').remove()
 
@@ -8,47 +9,43 @@ $(function() {
             var elClass = $this.attr('class')
             var message = ''
 
-            if (!elClass) {
-                return
-            }
-
-            if ($this.hasClass('required') && $this.val().length < 1) {
+            if ($this.data('required') && $this.val().length < 1) {
                 message += 'Required Field<br>'
             }
 
-            if (elClass.indexOf('min-') !== -1) {
-                var min = elClass.match(/min-(\d+)/)[1]
-                if (
-                    ($this.val().length < min && $this.hasClass('required'))
-                    ||
-                    ($this.val().length < min && $this.val().length > 0 && !$this.hasClass('required'))
-                ) {
-                    message += min + ' character minimum<br>'
-                }
-            }
-
-            if (elClass.indexOf('max-') !== -1) {
-                var max = elClass.match(/max-(\d+)/)[1]
+            if ($this.data('max')) {
+                var max = $this.data('max')
                 if ($this.val().length > max) {
                     message += max + ' characters maximum<br>'
                 }
             }
 
-            if (elClass.indexOf('alpha') !== -1) {
-                var regex = /^[A-z\s]+$/
-                if (!regex.test($this.val()) && $this.val().length > 0) {
-                    message += 'Letters only<br>'
+            if ($this.data('min')) {
+                var min = $this.data('min')
+                if (
+                    ($this.val().length < min && $this.data('required') == 'true')
+                    ||
+                    ($this.val().length < min && $this.val().length > 0 && !$this.data('required') != 'true')
+                ) {
+                    message += min + ' character minimum<br>'
                 }
             }
 
-            if (elClass.indexOf('conditional') !== -1) {
-                var targetId = elClass.match(/conditional-([A-z]+)/)[1]
-                var $target = $('#' + targetId)
-                var value = elClass.match(/conditional-[A-z]+-(.+)\s/)[1]
-                if ($target.is(':checked') && $this.val().length < 1) {
-                    message +=  'Required field while ' + targetId + ' is ' + value + '<br>'
-                }
-            }
+            //if (elClass.indexOf('alpha') !== -1) {
+            //    var regex = /^[A-z\s]+$/
+            //    if (!regex.test($this.val()) && $this.val().length > 0) {
+            //        message += 'Letters only<br>'
+            //    }
+            //}
+            //
+            //if (elClass.indexOf('conditional') !== -1) {
+            //    var targetId = elClass.match(/conditional-([A-z]+)/)[1]
+            //    var $target = $('#' + targetId)
+            //    var value = elClass.match(/conditional-[A-z]+-(.+)\s/)[1]
+            //    if ($target.is(':checked') && $this.val().length < 1) {
+            //        message +=  'Required field while ' + targetId + ' is ' + value + '<br>'
+            //    }
+            //}
 
             if (message.length > 0) {
                 submit = false
