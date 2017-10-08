0df0597a846dbf13d2f6498c38fdca3bfbc27b3d
Robbie Wood <githubphagocyte@gmail.com>
2017-10-08 20:28:08 +0100

    Add empty function bodies for form validation.

---
+++ validation.js
@@ -0,0 +1,43 @@
+window.onload = function() {
+    document.getElementById("required_field").addEventListener("change", validateRequiredField);
+    document.getElementById("max_length_8").addEventListener("change", validateMaxLength8);
+    document.getElementById("required_min_10_max_25").addEventListener("change", validateRequiredMin10Max25);
+    document.getElementById("not_required_min_10_max_25").addEventListener("change", validateNotRequiredMin10Max25);
+    document.getElementById("letters_only").addEventListener("change", validateLettersOnly);
+    document.getElementById("required_conditionally").addEventListener("change", validateRequiredConditionally);
+    document.getElementById("on_submit").addEventListener("change", validateOnSubmit);
+    document.getElementById("valid_email").addEventListener("change", validateEmail);
+}
+
+function validateRequiredField() {
+
+}
+
+function validateMaxLength8() {
+
+}
+
+function validateRequiredMin10Max25() {
+
+}
+
+function validateNotRequiredMin10Max25() {
+
+}
+
+function validateLettersOnly() {
+
+}
+
+function validateRequiredConditionally() {
+
+}
+
+function validateOnSubmit() {
+
+}
+
+function validateEmail() {
+
+}
+
